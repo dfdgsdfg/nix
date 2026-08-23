75e20a9bd54aba5b4c5e681e49c96b71da007c97
sg <dfdgsdfg@gmail.com>
2026-08-23 20:13:29 +0900

    home: update OMP to 18.0.3

--- home/hosts/nixos/lenovo-ideapadslim3/modules/ai.nix
+++ home/hosts/nixos/lenovo-ideapadslim3/modules/ai.nix
@@ -4,11 +4,11 @@ let
   system = pkgs.stdenv.hostPlatform.system;
   omp = pkgs.stdenvNoCC.mkDerivation rec {
     pname = "omp";
-    version = "17.4.2";
+    version = "18.0.3";
 
     src = pkgs.fetchurl {
       url = "https://github.com/can1357/oh-my-pi/releases/download/v${version}/omp-linux-x64";
-      hash = "sha256-IYqGhMKxEla0fii6ExrfsqA+mI7d2FZ72Da3xR3QIAU=";
+      hash = "sha256-RzNmIGK/tDZOSrOH15QOVqcUPl3BwgcmeH5huxtS3yg=";
     };
 
     dontUnpack = true;
