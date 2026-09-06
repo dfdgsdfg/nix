9c2ab6cd56ee6fc3777aecc013d4ca32eee1f0d8
sg <dfdgsdfg@gmail.com>
2026-09-06 09:43:31 +0900

    nix: add nix-community binary cache

--- system/profiles/darwin/base.nix
+++ system/profiles/darwin/base.nix
@@ -14,6 +14,10 @@
       "nix-command"
       "flakes"
     ];
+    customSettings.extra-substituters = [ "https://nix-community.cachix.org" ];
+    customSettings.extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
   };
 
   users.users.dididi = {
--- system/profiles/nixos/base.nix
+++ system/profiles/nixos/base.nix
@@ -1,7 +1,13 @@
 { inputs, pkgs, ... }:
 {
   nix.registry.unstable.flake = inputs.nixpkgs-unstable;
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings = {
+    experimental-features = [ "nix-command" "flakes" ];
+    extra-substituters = [ "https://nix-community.cachix.org" ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
   programs.fish.enable = true;
 
   users.users.dididi = {
